2908e4270eb07ee80a2c6914974761aaa664d087
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2025-09-08 16:29:02 +0800

    feat: add Apache License header to TimescaleNodeConfig and TimescaleWriterNode

--- src/main/java/com/solx/tb/timescale/TimescaleWriterNode.java
+++ src/main/java/com/solx/tb/timescale/TimescaleWriterNode.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2018-2025 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.solx.tb.timescale;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -10,7 +25,7 @@ import org.thingsboard.rule.engine.api.TbNodeConfiguration;
 import org.thingsboard.rule.engine.api.TbNodeException;
 import org.thingsboard.rule.engine.api.util.TbNodeUtils;
 
-import org.thingsboard.server.common.data.rule.ComponentType;
+import org.thingsboard.server.common.data.plugin.ComponentType;
 import org.thingsboard.server.common.msg.TbMsg;
 
 import javax.sql.DataSource;
@@ -20,7 +35,7 @@ import java.sql.Timestamp;
 import java.util.Iterator;
 
 @RuleNode(
-        type = ComponentType.ACTION,
+        type = ComponentType.TRANSFORMATION,
         name = "Timescale Writer",
         configClazz = TimescaleNodeConfig.class,
         nodeDescription = "Write incoming telemetry to TimescaleDB (PostgreSQL JDBC)",
